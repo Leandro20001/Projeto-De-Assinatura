274a0e739a9bd2fd7999535279c0cad17e68ed63
Leandro20001 <leandroferreira.dev@gmail.com>
2023-10-16 18:24:41 -0300

    Novo Projeto

---
+++ home.js
@@ -0,0 +1,7 @@
+const planContainer = document.querySelector('#PlanContainer')
+
+const startButton = document.querySelector('#mainButton')
+startButton.addEventListener('click',()=>{
+    planContainer.classList.remove('hide')
+    
+})
