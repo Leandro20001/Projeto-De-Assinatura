67cb7b9a87f8d0f8d15dc060a75a3cc504c16d90
Leandro20001 <leandroferreira.dev@gmail.com>
2023-10-17 15:50:53 -0300

    Updating

--- home.js
+++ home.js
@@ -1,7 +1,14 @@
 const planContainer = document.querySelector('#PlanContainer')
+const getCard = document.querySelectorAll('.planCard.hide')
+const firstDisplay = document.querySelector('#firstDisplay')
+
 
 const startButton = document.querySelector('#mainButton')
 startButton.addEventListener('click',()=>{
+    firstDisplay.classList.add()
     planContainer.classList.remove('hide')
+    getCard.forEach((c)=>{
+        c.classList.remove('hide')
+    })
     
 })
